f4576eb87de222f0359493a1292e38f3a87aec47
Matthieu-31 <matthieu.tinturier@gmail.com>
2023-02-17 21:48:43 +0100

    changement d'indentation

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const {Client, GatewayIntentBits} = require('discord.js');
-const {token} = require ('./config.json');
+const {Client, GatewayIntentBits, EmbedBuilder} = require('discord.js');
+const {token, prefix} = require ('./config.json');
 
 const client = new Client 
 ({ 
@@ -14,8 +14,13 @@ const client = new Client
 client.on('ready', () => {
     console.log("salute");
     client.user.setStatus("online");
-    client.user.setActivity("contrer la blague du 'feur'");
+    client.user.setActivity("à contrer la blague du 'feur'");
 });
+
+const exampleEmbed = new EmbedBuilder()
+.setColor(0xe510db)
+.setTitle('Bot anti-blague "feur"')
+.setDescription("J'ai créé ce petit bot, pour mettre fin au règne de la terreur que nous impose la blague du 'quoi ?' -Feur \n aucune commande à display, il ne sert qu'à contrer une blague trop répandue");
 
 client.on("messageCreate", message => {
 
@@ -30,12 +35,12 @@ client.on("messageCreate", message => {
     else if (message.content.toUpperCase()==="GORGE"){
         message.reply("profonde");
     }
-
 })
 
-const exampleEmbed = new EmbedBuilder()
-.setColor(0xe510db)
-.setTitle('Bot anti-blague "feur"')
-.setDescription("J'ai créé ce petit bot, pour mettre fin au règne de la terreur que nous impose la blague du 'quoi ?' -Feur \n aucune commande à display, il ne sert qu'à contrer une blague trop répandue");
+client.on("messageCreate", message=>{
+    if(message.content=== prefix+"help"){
+        message.channel.send({embeds: [exampleEmbed]});
+    }
+})
 
 client.login(token);
